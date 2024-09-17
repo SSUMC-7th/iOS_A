ab5105e73cc471ecd9a633d53b7f42cbacdce50e
Jiwoong CHOI <jwchoi179@soongsil.ac.kr>
2024-09-18 01:51:33 +0900

    [JC] Sample Xcode Project File

---
+++ jc/Week1/SampleProject/SampleProject/SampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleProjectApp.swift
+//  SampleProject
+//
+//  Created by Jiwoong CHOI on 9/18/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
