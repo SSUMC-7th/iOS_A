e4bf8a09d78404d7bb512c19f562b4430fe1fae6
taebin2 <taebin325@gmail.com>
2024-10-05 01:43:47 +0900

    1주차 과제 초안 업로드

---
+++ Week1/UMCKream/UMCKreamTests/UMCKreamTests.swift
@@ -0,0 +1,17 @@
+//
+//  UMCKreamTests.swift
+//  UMCKreamTests
+//
+//  Created by 한태빈 on 9/30/24.
+//
+
+import Testing
+@testable import UMCKream
+
+struct UMCKreamTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
