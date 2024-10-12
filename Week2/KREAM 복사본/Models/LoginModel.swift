356104a6121d7a25102a83512d6a08af26ce3372
taebin2 <taebin325@gmail.com>
2024-10-12 21:42:09 +0900

    haetae/week2 추가

---
+++ Week2/KREAM 복사본/Models/LoginModel.swift
@@ -0,0 +1,12 @@
+//
+//  LoginModel.swift
+//  MyKream
+//
+//  Created by 한태빈 on 9/28/24.
+//id, pwd 를 담당하는 변수를 관리하는 데이터 구조를 LoginModel.swift에 구현해주세요
+import Foundation
+
+struct LoginModel{
+    var id: String = ""
+    var pwd: String = ""
+}
