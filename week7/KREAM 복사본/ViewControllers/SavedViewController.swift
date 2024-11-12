0f00b575372e3a551c5362f4f86113d87f07777b
taebin2 <taebin325@gmail.com>
2024-11-13 00:01:45 +0900

    [haetae/week7] 7주차 과제

---
+++ week7/KREAM 복사본/ViewControllers/SavedViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SavedViewController.swift
+//  UMCKream
+//
+//  Created by 한태빈 on 10/6/24.
+//
+
+import UIKit
+
+class SavedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    
+    }
+}
